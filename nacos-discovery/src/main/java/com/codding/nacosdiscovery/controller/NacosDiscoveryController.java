e858c4968225875ae3cc424e9bf2683e266ddc5b
han910917 <han910917@163.com>
2020-11-03 19:42:19 +0800

    修改nacos discovery的结构

--- nacos-discovery/src/main/java/com/codding/nacosdiscovery/controller/NacosDiscoveryController.java
+++ nacos-discovery/src/main/java/com/codding/nacosdiscovery/controller/NacosDiscoveryController.java
@@ -1,5 +1,7 @@
 package com.codding.nacosdiscovery.controller;
 
+import com.codding.nacosdiscovery.service.NacosDiscoveryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +17,11 @@ public class NacosDiscoveryController {
     @Value("${server.port}")
     private String port;
 
+    @Autowired
+    private NacosDiscoveryService nacosDiscoveryService;
+
     @GetMapping(value = "/echo")
     public String echo(){
-        return "服务端口是: " + port;
+        return nacosDiscoveryService.echo();
     }
 }
