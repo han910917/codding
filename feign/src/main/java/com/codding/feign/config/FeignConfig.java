74e2a549bf374858660027e61517cb36e536e205
han910917 <han910917@163.com>
2020-11-04 19:36:06 +0800

    添加注释和修改pom文件

--- feign/src/main/java/com/codding/feign/config/FeignConfig.java
+++ feign/src/main/java/com/codding/feign/config/FeignConfig.java
@@ -5,5 +5,7 @@ package com.codding.feign.config;
  * @Author hgm
  * @Time 2020/11/3 19:12
  */
+// @Configuration 不用使用，如果要使用的的话，不能放到主程序所在的包，只能放到兄弟包下
+// 原因 https://docs.spring.io/spring-cloud-openfeign/docs/2.2.5.RELEASE/reference/html/ 1.2注意提示信息
 public class FeignConfig {
 }
